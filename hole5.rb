9fe32f759c2f06fbac95764b039494a685be7845
Dan Actor <danltactor@gmail.com>
2018-01-19 11:02:22 -0700

    complete hole 5

---
+++ hole5.rb
@@ -0,0 +1,11 @@
+# Take in a string and an offset, encrypt the string by moving letters over by the offset  
+# return the encrypted string (e.g “ab”, 3 would return “de”). When you hit Z loop back to A.
+
+def cipher
+  s = STDIN.gets.strip.downcase.gsub(" ", "")
+  o = STDIN.gets.strip.to_i
+  a = Array("a".."z")
+  e = s.chars.map { |char| a[a.index(char) + o - a.length] }.join
+  puts e
+end
+cipher
