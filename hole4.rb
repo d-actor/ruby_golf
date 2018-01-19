68f22fbe71341913012429798cafbaa45bc99347
Dan Actor <danltactor@gmail.com>
2018-01-19 10:48:18 -0700

    complete hole 4

--- hole4.rb
+++ hole4.rb
@@ -1,2 +1,16 @@
 # takes in a number and a max value then finds all multiples of the number up to the max value.
 
+def multiples
+  n = STDIN.gets.strip.to_i
+  m = STDIN.gets.strip.to_i
+  loop do
+    if (m > 0)
+    puts n * m
+    m -= 1
+    else
+      break
+    end
+  end
+end
+
+multiples
