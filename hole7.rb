bebf5fcd78a821292d426aabd03d19ffd296355b
Dan Actor <danltactor@gmail.com>
2018-02-02 12:02:00 -0700

    complete hole 7

--- hole7.rb
+++ hole7.rb
@@ -1,12 +1,16 @@
-# The method takes in an array of arrays. Each inner array contains a pair
-# The method then mixes up the pairs and returns a new array with the pairs mixed up.
 require "pry"
 
 def mixed_pairs
-  a = []
-  str = STDIN.gets.strip.gsub(" ", "").split("")
-  str.each do |l|
-    
-  end 
+  a = [['a', 'b'], ['c', 'd']]
+  m1 = []
+  m2 = []
+  m = []
+  a.each do |i|
+    m1 << i[0]
+    m2 << i[1]
+  end
+  m << m1
+  m << m2
+  print m
 end
 mixed_pairs
