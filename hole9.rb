b38b5e0fa827ca838d4feddfec57befefacb7330
Dan Actor <danltactor@gmail.com>
2018-02-02 12:57:16 -0700

    complete hole 9

--- hole9.rb
+++ hole9.rb
@@ -1,2 +1,15 @@
 # Takes in a list of strings. Program sorts the list non case sensitive 
 # Removes duplicates and returns as hash using position in the list as priority.
+require 'pry'
+
+def shopping_list
+  x = 1
+  shop = []
+  list = STDIN.gets.strip.split(" ").uniq.sort
+  list.each do |l|
+    shop << Hash[x, l]
+    x += 1
+  end
+  print shop
+end
+shopping_list
